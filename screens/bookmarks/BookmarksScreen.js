46b6f91ac99d04390f348718a9379ea7228cb88c
Jul1enF <j.furic@gmail.com>
2024-09-15 15:49:09 +0200

    test 5 déploiement

--- screens/bookmarks/BookmarksScreen.js
+++ screens/bookmarks/BookmarksScreen.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 
 export default function BookmarksScreen ({navigation}) {
     const [firstname, setFirstname]= useState('')
-    const [result, setResult]=useState('')
+
     const url = process.env.EXPO_PUBLIC_BACK_ADDRESS
 
     const postClick = async()=>{
@@ -14,16 +14,12 @@ export default function BookmarksScreen ({navigation}) {
             })
         const data = await response.json()
         console.log(data)
-        if (!data.result){
-            setResult(data)
-        }
     }
     return (
         <View>
         <Text> HELLO IT'S BOOKMARKKS</Text>
         <TextInput style={{width : 200, backgroundColor: "red"}} onChangeText={(e)=>setFirstname(e)} value={firstname}></TextInput>
         <Button type='text' onPress={()=>postClick()} title="POST"></Button>
-        <Text>{result}</Text>
         </View>
     )
 }
