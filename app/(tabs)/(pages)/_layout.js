f4421030f6cb2f3a157e94007dfd505171541e7c
Jul1enF <j.furic@gmail.com>
2025-07-03 16:38:23 +0200

    WIP passage expo 53

--- app/(tabs)/(pages)/_layout.js
+++ app/(tabs)/(pages)/_layout.js
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
+import { RPH, RPW } from "../../../modules/dimensions"
+import { Header } from "@react-navigation/elements"
 
 export default function RootLayout() {
     return (
@@ -12,34 +14,48 @@ export default function RootLayout() {
             <Stack.Screen name="users" />
             <Stack.Screen name="user-informations" />
             <Stack.Screen name="home" options={{
-                title : "Accueil"
-            }}/>
+                title: "Accueil"
+            }} />
             <Stack.Screen name="legal" options={{
-                headerShown : true,
-                title : "CGU / Mentions Légales",
-                headerTintColor: 'white',
-                headerBackground: () => (
-                    <LinearGradient
-                      colors={['#9dcb00', '#045400']}
-                      start={{ x: 0, y: 0.5 }}
-                      end={{ x: 1, y: 0.5 }}
-                      style={{ height: 150 }}
-                    ></LinearGradient>
-                  ),
+                headerShown: true,
+                title: "CGU / Mentions Légales",
+                headerTintColor: 'black',
+                headerStyle:{
+                    backgroundColor : "red",
+                },
+                headerTopInsetEnabled : true,
+                // headerStyle: {
+                //     height: 80,
+                // },
+                // header: ({ options }) => (
+                //     <Header
+                //         {...options}
+                //     />
+                // ),
+                // headerBackgroundContainerStyle : {height : 150, alignItems : "flex-start", justifyContent : "flex-start", padding : 0, margin : 0},
+                // headerStatusBarHeight : 0,
+                // headerBackground: () => (
+                //     <LinearGradient
+                //         colors={['#9dcb00', '#045400']}
+                //         start={{ x: 0, y: 0.5 }}
+                //         end={{ x: 1, y: 0.5 }}
+                //         style={{ height: RPH(6) }}
+                //     />
+                // ),
             }} />
             <Stack.Screen name="contact" options={{
-                headerShown : true,
-                title : "Contact",
+                headerShown: true,
+                title: "Contact",
                 headerTintColor: 'white',
                 headerBackground: () => (
                     <LinearGradient
-                      colors={['#9dcb00', '#045400']}
-                      start={{ x: 0, y: 0.5 }}
-                      end={{ x: 1, y: 0.5 }}
-                      style={{ height: 150 }}
+                        colors={['#9dcb00', '#045400']}
+                        start={{ x: 0, y: 0.5 }}
+                        end={{ x: 1, y: 0.5 }}
+                        style={{ height: 150 }}
                     ></LinearGradient>
-                  ),
-            }}/>
+                ),
+            }} />
         </Stack>
     )
 }
--- app/_layout.js
+++ app/_layout.js
@@ -52,39 +52,34 @@ export default function RootLayout() {
 
     Notifications.setNotificationHandler({
         handleNotification: async () =>
-            ({
-                shouldShowAlert: true,
-                shouldPlaySound: true,
-                shouldSetBadge: true,
-            })
+        ({
+            shouldShowAlert: true,
+            shouldPlaySound: true,
+            shouldSetBadge: true,
+        })
     });
 
- 
+
     // Hooks pour l'écoute et l'enregistrement des notifications
 
     const [notification, setNotification] = useState('');
-    
-    const notificationListener = useRef('');
-    const responseListener = useRef('');
+
 
 
     useEffect(() => {
         // Écoute et enregistrement des notifactions. Démontage des listeners
 
-        notificationListener.current = Notifications.addNotificationReceivedListener(notif => {
-            setNotification(notifications => [...notifications, notif]);
-            console.log("NOTIF", notif)
+        const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
         });
 
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log("RESPONSE :", response);
+        const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
         });
 
         return () => {
-            notificationListener.current &&
-                Notifications.removeNotificationSubscription(notificationListener.current);
-            responseListener.current &&
-                Notifications.removeNotificationSubscription(responseListener.current);
+            notificationListener.remove();
+            responseListener.remove();
         };
     }, [notification])
 
@@ -94,41 +89,41 @@ export default function RootLayout() {
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <KeyboardProvider>
-                <Stack >
-                    <Stack.Screen name="index" options={{
-                        headerShown: false,
-                        title: "Connexion",
-                    }} />
-                    <Stack.Screen name="(tabs)" options={{
-                        headerShown: false,
-                    }} />
-                    <Stack.Screen name="legal-index" options={{
-                        headerShown: true,
-                        title: "CGU / Mentions Légales",
-                        headerTintColor: 'white',
-                        headerBackground: () => (
-                            <LinearGradient
-                                colors={['#9dcb00', '#045400']}
-                                start={{ x: 0, y: 0.5 }}
-                                end={{ x: 1, y: 0.5 }}
-                                style={{ height: 150 }}
-                            ></LinearGradient>
-                        ),
-                    }} />
-                    <Stack.Screen name="contact-index" options={{
-                        headerShown: true,
-                        title: "Contact",
-                        headerTintColor: 'white',
-                        headerBackground: () => (
-                            <LinearGradient
-                                colors={['#9dcb00', '#045400']}
-                                start={{ x: 0, y: 0.5 }}
-                                end={{ x: 1, y: 0.5 }}
-                                style={{ height: 150 }}
-                            ></LinearGradient>
-                        ),
-                    }} />
-                </Stack>
+                    <Stack >
+                        <Stack.Screen name="index" options={{
+                            headerShown: false,
+                            title: "Connexion",
+                        }} />
+                        <Stack.Screen name="(tabs)" options={{
+                            headerShown: false,
+                        }} />
+                        <Stack.Screen name="legal-index" options={{
+                            headerShown: true,
+                            title: "CGU / Mentions Légales",
+                            headerTintColor: 'white',
+                            headerBackground: () => (
+                                <LinearGradient
+                                    colors={['#9dcb00', '#045400']}
+                                    start={{ x: 0, y: 0.5 }}
+                                    end={{ x: 1, y: 0.5 }}
+                                    style={{ height: 150 }}
+                                ></LinearGradient>
+                            ),
+                        }} />
+                        <Stack.Screen name="contact-index" options={{
+                            headerShown: true,
+                            title: "Contact",
+                            headerTintColor: 'white',
+                            headerBackground: () => (
+                                <LinearGradient
+                                    colors={['#9dcb00', '#045400']}
+                                    start={{ x: 0, y: 0.5 }}
+                                    end={{ x: 1, y: 0.5 }}
+                                    style={{ height: 150 }}
+                                ></LinearGradient>
+                            ),
+                        }} />
+                    </Stack>
                 </KeyboardProvider>
             </PersistGate>
         </Provider>
